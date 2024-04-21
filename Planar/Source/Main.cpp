577af9c53873ad6835bfc93bc49cb20977000431
Rageking8 <tomleetyt@gmail.com>
2024-04-21 22:05:56 +0800

    Add basic `GLFW` init and window creation logic

--- Planar/Source/Main.cpp
+++ Planar/Source/Main.cpp
@@ -4,5 +4,29 @@
 
 int main()
 {
-    std::cout << "Hello World!";
+    if (!glfwInit())
+    {
+        std::cerr << "GLFW init failed\n";
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(1280, 720, "Hello Window",
+        nullptr, nullptr);
+    if (!window)
+    {
+        std::cerr << "Window creation failed\n";
+    }
+
+    glfwMakeContextCurrent(window);
+
+    glfwSwapInterval(1);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
 }
