6710078925d663250b4dd27b1af5fb47e15a60bf
Rageking8 <tomleetyt@gmail.com>
2024-04-27 23:14:29 +0800

    Create `Time` utilities

---
+++ Include/Planar/Engine/Core/Utils/Time/Time.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "Planar/Engine/Core/ExportMacros.hpp"
+
+#include <string>
+
+namespace Planar::Engine::Core::Utils::Time
+{
+    enum class Format
+    {
+        // HH:MM:SS
+        HHMMSS,
+
+        // DD/MM/YYYY
+        DDMMYYYY,
+
+        // DD/MM/YYYY HH:MM:SS
+        DDMMYYYYHHMMSS,
+    };
+
+    PLANAR_API std::string get_time_string(Format format);
+}
---
+++ Source/Planar/Engine/Core/Utils/Time/Time.cpp
@@ -0,0 +1,33 @@
+#include "Planar/Engine/Core/Utils/Time/Time.hpp"
+
+#include <ctime>
+#include <chrono>
+#include <iomanip>
+#include <sstream>
+
+namespace Planar::Engine::Core::Utils::Time
+{
+    std::string get_time_string(Format format)
+    {
+        const std::time_t time = std::time(nullptr);
+        const tm* local_time = std::localtime(&time);
+        std::ostringstream output;
+
+        switch (format)
+        {
+        case Format::HHMMSS:
+            output << std::put_time(local_time, "%H:%M:%S");
+            break;
+
+        case Format::DDMMYYYY:
+            output << std::put_time(local_time, "%d/%m/%Y");
+            break;
+
+        case Format::DDMMYYYYHHMMSS:
+            output << std::put_time(local_time, "%d/%m/%Y %H:%M:%S");
+            break;
+        }
+
+        return output.str();
+    }
+}
