c7a188a878457bf0209f855d682d4cd78d1a108a
Rageking8 <tomleetyt@gmail.com>
2024-04-29 00:27:13 +0800

    Add `UnsignedEnumClass` concept

--- Include/Planar/Engine/Core/Utils/Concepts/EnumClass.hpp
+++ Include/Planar/Engine/Core/Utils/Concepts/EnumClass.hpp
@@ -2,9 +2,15 @@
 
 #include "Planar/Engine/Core/Utils/TypeTraits/IsEnumClass.hpp"
 
+#include <type_traits>
+
 namespace Planar::Engine::Core::Utils::Concepts
 {
     template <typename T>
     concept EnumClass =
         Planar::Engine::Core::Utils::TypeTraits::IsEnumClassV<T>;
+
+    template <typename T>
+    concept UnsignedEnumClass =
+        EnumClass<T> && std::is_unsigned_v<std::underlying_type_t<T>>;
 }
