5e0aabe654d895ca4d2553bb7aa94b8029f5729d
Rageking8 <tomleetyt@gmail.com>
2024-05-01 03:20:22 +0800

    Augment `pack_text_to_string` for multi file inputs

--- CMake/PackFiles.cmake
+++ CMake/PackFiles.cmake
@@ -1,17 +1,18 @@
 # Convert `source_file` from text to a std::string and place
 # it in a new C++ file, before adding to `target` as source
-function(pack_text_to_string source_file target)
+function(pack_text_to_string target)
     file(RELATIVE_PATH namespace ${CMAKE_SOURCE_DIR} ${CMAKE_CURRENT_SOURCE_DIR})
     string(REPLACE "/" "::" namespace ${namespace})
 
-    get_filename_component(source_name ${source_file} NAME_WLE)
-    set(output_file ${CMAKE_CURRENT_BINARY_DIR}/${source_name}.cpp)
+    foreach (source_file ${ARGN})
+        get_filename_component(source_name ${source_file} NAME_WLE)
+        set(output_file ${CMAKE_CURRENT_BINARY_DIR}/${source_name}.cpp)
 
-    file(READ ${source_file} file_data)
-    string(REPLACE "\n" "\\n" file_data ${file_data})
-    string(REPLACE "\"" "\\\"" file_data ${file_data})
+        file(READ ${source_file} file_data)
+        string(REPLACE "\n" "\\n" file_data ${file_data})
+        string(REPLACE "\"" "\\\"" file_data ${file_data})
 
-    file(WRITE ${output_file}
+        file(WRITE ${output_file}
 "#include <string>
 
 namespace Planar::${namespace}
@@ -19,7 +20,8 @@ namespace Planar::${namespace}
     extern const std::string ${source_name} = \"${file_data}\";
 }
 "
-    )
+        )
 
-    target_sources(${target} PRIVATE ${output_file})
+        target_sources(${target} PRIVATE ${output_file})
+    endforeach()
 endfunction()
