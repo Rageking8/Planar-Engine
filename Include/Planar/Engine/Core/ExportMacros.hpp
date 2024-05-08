9f8268e1472c688762290dbcc4272f422c1c17c1
Rageking8 <tomleetyt@gmail.com>
2024-05-08 16:56:30 +0800

    Split construct and destruct export macros

--- Include/Planar/Engine/Core/ExportMacros.hpp
+++ Include/Planar/Engine/Core/ExportMacros.hpp
@@ -10,17 +10,28 @@
     #define PLANAR_API
 #endif
 
-#define PLANAR_DECLARE_CONSTRUCT_DESTRUCT(namespace_prefix)    \
-    PLANAR_API void* namespace_prefix##_construct();           \
+#define PLANAR_DECLARE_CONSTRUCT(namespace_prefix)   \
+    PLANAR_API void* namespace_prefix##_construct(); \
+
+#define PLANAR_DECLARE_DESTRUCT(namespace_prefix)              \
     PLANAR_API void namespace_prefix##_destruct(void* handle); \
 
+#define PLANAR_DECLARE_CONSTRUCT_DESTRUCT(namespace_prefix) \
+    PLANAR_DECLARE_CONSTRUCT(namespace_prefix)              \
+    PLANAR_DECLARE_DESTRUCT(namespace_prefix)               \
+
+#define PLANAR_DEFINE_CONSTRUCT(namespace_prefix, type) \
+    void* namespace_prefix##_construct()                \
+    {                                                   \
+        return new type;                                \
+    }                                                   \
+
+#define PLANAR_DEFINE_DESTRUCT(namespace_prefix, type) \
+    void namespace_prefix##_destruct(void* handle)     \
+    {                                                  \
+        delete handle;                                 \
+    }                                                  \
+
 #define PLANAR_DEFINE_CONSTRUCT_DESTRUCT(namespace_prefix, type) \
-    void* namespace_prefix##_construct()                         \
-    {                                                            \
-        return new type;                                         \
-    }                                                            \
-                                                                 \
-    void namespace_prefix##_destruct(void* handle)               \
-    {                                                            \
-        delete handle;                                           \
-    }                                                            \
+    PLANAR_DEFINE_CONSTRUCT(namespace_prefix, type)              \
+    PLANAR_DEFINE_DESTRUCT(namespace_prefix, type)               \
