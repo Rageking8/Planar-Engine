0d1331f2a9fb55f05555d76f49760b0b4665de1e
Rageking8 <tomleetyt@gmail.com>
2024-05-09 19:23:40 +0800

    Create constructor for `Size2D` C# struct

--- Script/Planar/Engine/Math/Size2D.cs
+++ Script/Planar/Engine/Math/Size2D.cs
@@ -6,6 +6,12 @@ namespace Planar.Engine.Math;
 [StructLayout(LayoutKind.Sequential)]
 public struct Size2D<T> where T : INumber<T>
 {
+    public Size2D(T width, T height)
+    {
+        this.width = width;
+        this.height = height;
+    }
+
     public T width;
     public T height;
 }
