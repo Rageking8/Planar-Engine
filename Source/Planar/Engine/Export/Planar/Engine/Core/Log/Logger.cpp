980b7e0e5458ac292bbea10d4ba5378fb2954a50
Rageking8 <tomleetyt@gmail.com>
2024-05-10 18:37:53 +0800

    Use new `PLANAR_EXPORT_DEFINE_HANDLE_FUNCTION` overloads in `Logger.cpp`

--- Source/Planar/Engine/Export/Planar/Engine/Core/Log/Logger.cpp
+++ Source/Planar/Engine/Export/Planar/Engine/Core/Log/Logger.cpp
@@ -4,11 +4,11 @@
 PLANAR_EXPORT_DEFINE_CONSTRUCT_DESTRUCT(Planar_Engine_Core_Log_Logger,
     Planar::Engine::Core::Log::Logger)
 
-PLANAR_EXPORT_DEFINE_HANDLE_FUNCTION(Planar_Engine_Core_Log_Logger, log,
-    void, const char* text, text, Planar::Engine::Core::Log::Logger)
-PLANAR_EXPORT_DEFINE_HANDLE_FUNCTION(Planar_Engine_Core_Log_Logger, warn,
-    void, const char* text, text, Planar::Engine::Core::Log::Logger)
-PLANAR_EXPORT_DEFINE_HANDLE_FUNCTION(Planar_Engine_Core_Log_Logger, error,
-    void, const char* text, text, Planar::Engine::Core::Log::Logger)
-PLANAR_EXPORT_DEFINE_HANDLE_FUNCTION(Planar_Engine_Core_Log_Logger, success,
-    void, const char* text, text, Planar::Engine::Core::Log::Logger)
+PLANAR_EXPORT_DEFINE_HANDLE_FUNCTION(Planar_Engine_Core_Log_Logger,
+    log, const char* text, text, Planar::Engine::Core::Log::Logger)
+PLANAR_EXPORT_DEFINE_HANDLE_FUNCTION(Planar_Engine_Core_Log_Logger,
+    warn, const char* text, text, Planar::Engine::Core::Log::Logger)
+PLANAR_EXPORT_DEFINE_HANDLE_FUNCTION(Planar_Engine_Core_Log_Logger,
+    error, const char* text, text, Planar::Engine::Core::Log::Logger)
+PLANAR_EXPORT_DEFINE_HANDLE_FUNCTION(Planar_Engine_Core_Log_Logger,
+    success, const char* text, text, Planar::Engine::Core::Log::Logger)
