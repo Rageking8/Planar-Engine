d88e6931565464af38547b2e0bfb8398fdfca854
Rageking8 <tomleetyt@gmail.com>
2024-05-12 22:40:52 +0800

    Create basic `Color` struct

---
+++ Include/Planar/Engine/Graphics/Color.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "Planar/Engine/Export/ExportMacros.hpp"
+
+namespace Planar::Engine::Graphics
+{
+    struct PLANAR_API Color
+    {
+        float r{};
+        float g{};
+        float b{};
+        float a{};
+    };
+}
