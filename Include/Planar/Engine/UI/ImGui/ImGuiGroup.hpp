5c62d13e27f49e79d3fd1d50692f81c98c4e0991
Rageking8 <tomleetyt@gmail.com>
2024-05-14 18:02:55 +0800

    Use new wrapper macros for `ImGuiGroup`

--- Include/Planar/Engine/UI/ImGui/ImGuiGroup.hpp
+++ Include/Planar/Engine/UI/ImGui/ImGuiGroup.hpp
@@ -1,13 +1,9 @@
 #pragma once
 
 #include "Planar/Engine/Export/ExportMacros.hpp"
+#include "Planar/Engine/UI/ImGui/Macros/WrapperMacros.hpp"
 
 namespace Planar::Engine::UI::ImGui
 {
-    class PLANAR_API ImGuiGroup
-    {
-    public:
-        ImGuiGroup();
-        ~ImGuiGroup();
-    };
+    PLANAR_DECLARE_IMGUI_WRAPPER(ImGuiGroup)
 }
--- Source/Planar/Engine/UI/ImGui/ImGuiGroup.cpp
+++ Source/Planar/Engine/UI/ImGui/ImGuiGroup.cpp
@@ -1,16 +1,9 @@
 #include "Planar/Engine/UI/ImGui/ImGuiGroup.hpp"
+#include "Planar/Engine/UI/ImGui/Macros/WrapperMacros.hpp"
 
 #include "ThirdParty/ImGui/imgui.h"
 
 namespace Planar::Engine::UI::ImGui
 {
-    ImGuiGroup::ImGuiGroup()
-    {
-        ::ImGui::BeginGroup();
-    }
-
-    ImGuiGroup::~ImGuiGroup()
-    {
-        ::ImGui::EndGroup();
-    }
+    PLANAR_DEFINE_IMGUI_WRAPPER(ImGuiGroup, BeginGroup, EndGroup)
 }
