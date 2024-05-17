05f7c56a10d9118c3b7b5dafa0b243150482a0dc
Rageking8 <tomleetyt@gmail.com>
2024-05-18 02:47:50 +0800

    Add loading macros for unsigned char array assets

--- Include/Planar/Engine/Asset/LoadAssetMacros.hpp
+++ Include/Planar/Engine/Asset/LoadAssetMacros.hpp
@@ -1,7 +1,46 @@
 #pragma once
 
+#include "Planar/Engine/Core/Utils/Macros/RepeatMacros.hpp"
+#include "Planar/Engine/Core/FileSystem/FileSystem.hpp"
+
+#include <cstddef>
+
 #define PLANAR_LOAD_STD_STRING_ASSET(ns, name) \
     namespace Planar::Asset::ns                \
     {                                          \
         extern const std::string name;         \
     }
+
+#define PLANAR_LOAD_UNSIGNED_CHAR_ARRAY_ASSET(ns, name) \
+    namespace Planar::Asset::ns                         \
+    {                                                   \
+        extern const unsigned char name[];              \
+        extern const std::size_t name_length;           \
+    }                                                   \
+
+#define PLANAR_LOAD_UNSIGNED_CHAR_ARRAY_ASSET_SINGLE_CHUNK( \
+    ns, name, chunk_id)                                     \
+    namespace Planar::Asset::ns                             \
+    {                                                       \
+        extern const unsigned char name##chunk_id[];        \
+        extern const std::size_t name##chunk_id##_length;   \
+    }                                                       \
+
+#define PLANAR_LOAD_UNSIGNED_CHAR_ARRAY_ASSET_ALL_CHUNKS(   \
+    ns, name)                                               \
+    PLANAR_REPEAT_MACRO_WITH_TRAILING_NUMBER(               \
+        PLANAR_ASSET_INCLUDE_##name,                        \
+        PLANAR_LOAD_UNSIGNED_CHAR_ARRAY_ASSET_SINGLE_CHUNK, \
+        ns, name)                                           \
+
+#define PLANAR_APPEND_SINGLE_CHUNK_TO_FILE(path, ns, name,  \
+    chunk_id)                                               \
+    Planar::Engine::Core::FileSystem::append_array_to_file( \
+        path, Planar::Asset::ns::name##chunk_id,            \
+        Planar::Asset::ns::name##chunk_id##_length);        \
+
+#define PLANAR_APPEND_ALL_CHUNKS_TO_FILE(path, ns, name) \
+    PLANAR_REPEAT_MACRO_WITH_TRAILING_NUMBER(            \
+        PLANAR_ASSET_INCLUDE_##name,                     \
+        PLANAR_APPEND_SINGLE_CHUNK_TO_FILE,              \
+        path, ns, name)                                  \
