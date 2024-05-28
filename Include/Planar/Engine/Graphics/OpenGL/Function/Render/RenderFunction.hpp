d5cd9674d912fce61c0d66dec4aafc5c7313e2f0
Rageking8 <tomleetyt@gmail.com>
2024-05-28 11:43:07 +0800

    Add `color_clear` function to `RenderFunction`

--- Include/Planar/Engine/Graphics/OpenGL/Function/Render/RenderFunction.hpp
+++ Include/Planar/Engine/Graphics/OpenGL/Function/Render/RenderFunction.hpp
@@ -1,12 +1,15 @@
 #pragma once
 
 #include "Planar/Engine/Export/ExportMacros.hpp"
+#include "Planar/Engine/Graphics/Color.hpp"
 #include "Planar/Engine/Graphics/OpenGL/Render/Primitive.hpp"
 
 #include "ThirdParty/glad/gl.h"
 
 namespace Planar::Engine::Graphics::OpenGL::Function::Render
 {
+    PLANAR_API void color_clear(Graphics::Color color);
+
     PLANAR_API void draw_arrays(OpenGL::Render::Primitive primitive,
         GLint first, GLsizei count);
 }
--- Source/Planar/Engine/Graphics/OpenGL/Function/Render/RenderFunction.cpp
+++ Source/Planar/Engine/Graphics/OpenGL/Function/Render/RenderFunction.cpp
@@ -2,6 +2,12 @@
 
 namespace Planar::Engine::Graphics::OpenGL::Function::Render
 {
+    void color_clear(Graphics::Color color)
+    {
+        glClearColor(color.r, color.g, color.b, color.a);
+        glClear(GL_COLOR_BUFFER_BIT);
+    }
+
     void draw_arrays(OpenGL::Render::Primitive primitive,
         GLint first, GLsizei count)
     {
