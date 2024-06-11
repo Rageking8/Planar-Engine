de093f4e1b4dd7256651de7c74e65df66a83783c
Rageking8 <tomleetyt@gmail.com>
2024-06-11 15:22:18 +0800

    Implement `set_indent_spacing` and `set_item_spacing` in `StyleVar`

--- Include/Planar/Engine/UI/ImGui/Style/StyleVar.hpp
+++ Include/Planar/Engine/UI/ImGui/Style/StyleVar.hpp
@@ -11,10 +11,16 @@ namespace Planar::Engine::UI::ImGui::Style
     public:
         ~StyleVar();
 
+        void set_indent_spacing(float indent_spacing);
+        void set_item_spacing(Math::Size2Df item_spacing);
         void set_frame_padding(Math::Size2Df padding);
         void set_window_padding(Math::Size2Df padding);
         void set_min_window_size(Math::Size2Df min_window_size);
 
         virtual void pop() override;
+
+    private:
+        void push_style_var(int type, float value);
+        void push_style_var(int type, Math::Size2Df value);
     };
 }
--- Source/Planar/Engine/UI/ImGui/Style/StyleVar.cpp
+++ Source/Planar/Engine/UI/ImGui/Style/StyleVar.cpp
@@ -9,30 +9,46 @@ namespace Planar::Engine::UI::ImGui::Style
         pop();
     }
 
+    void StyleVar::set_indent_spacing(float indent_spacing)
+    {
+        push_style_var(ImGuiStyleVar_IndentSpacing, indent_spacing);
+    }
+
+    void StyleVar::set_item_spacing(Math::Size2Df item_spacing)
+    {
+        push_style_var(ImGuiStyleVar_ItemSpacing, item_spacing);
+    }
+
     void StyleVar::set_frame_padding(Math::Size2Df padding)
     {
-        ::ImGui::PushStyleVar(ImGuiStyleVar_FramePadding,
-            { padding.width, padding.height });
-        push_count++;
+        push_style_var(ImGuiStyleVar_FramePadding, padding);
     }
 
     void StyleVar::set_window_padding(Math::Size2Df padding)
     {
-        ::ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding,
-            { padding.width, padding.height });
-        push_count++;
+        push_style_var(ImGuiStyleVar_WindowPadding, padding);
     }
 
     void StyleVar::set_min_window_size(Math::Size2Df min_window_size)
     {
-        ::ImGui::PushStyleVar(ImGuiStyleVar_WindowMinSize,
-            { min_window_size.width, min_window_size.height });
-        push_count++;
+        push_style_var(ImGuiStyleVar_WindowMinSize, min_window_size);
     }
 
     void StyleVar::pop()
     {
         ::ImGui::PopStyleVar(push_count);
         push_count = 0;
+    }
+
+    void StyleVar::push_style_var(int type, float value)
+    {
+        ::ImGui::PushStyleVar(type, value);
+        push_count++;
+    }
+
+    void StyleVar::push_style_var(int type, Math::Size2Df value)
+    {
+        ::ImGui::PushStyleVar(type, { value.width, value.height });
+        push_count++;
     }
 }
