9cdff1368b33416cc9e8c0e479e0dcbabbe4436c
Rageking8 <tomleetyt@gmail.com>
2024-06-20 02:02:33 +0800

    Augment `ProgressDisplay` with layout mode and offset

--- Include/Planar/Editor/UI/Element/ProgressDisplay.hpp
+++ Include/Planar/Editor/UI/Element/ProgressDisplay.hpp
@@ -12,18 +12,30 @@ namespace Planar::Editor::UI::Element
         public Engine::UI::ImGui::Element::Element
     {
     public:
-        ProgressDisplay();
+        enum class LayoutMode
+        {
+            VIEWPORT,
+
+            WINDOW,
+        };
+
+        ProgressDisplay(LayoutMode layout_mode = LayoutMode::VIEWPORT);
 
         virtual void render() override;
 
+        void set_layout_mode(LayoutMode new_layout_mode);
         void set_text(const std::string& new_text);
+        void set_y_offset(float new_y_offset);
         void increment(unsigned delta = 1);
         void reset(unsigned max);
 
     private:
+        LayoutMode layout_mode;
+
         Engine::UI::ImGui::Element::ProgressBar progress_bar;
         Engine::UI::ImGui::Renderer::TextRenderer text_renderer;
 
         std::string text;
+        float y_offset;
     };
 }
--- Source/Planar/Editor/UI/Element/ProgressDisplay.cpp
+++ Source/Planar/Editor/UI/Element/ProgressDisplay.cpp
@@ -4,7 +4,8 @@
 
 namespace Planar::Editor::UI::Element
 {
-    ProgressDisplay::ProgressDisplay()
+    ProgressDisplay::ProgressDisplay(LayoutMode layout_mode) :
+        layout_mode{ layout_mode }, y_offset{}
     {
 
     }
@@ -13,20 +14,57 @@ namespace Planar::Editor::UI::Element
     {
         using namespace Planar::Engine::UI;
 
-        ImGui::Core::Cursor::set_y_bottom_viewport(
-            -progress_bar.get_height() - 40.f);
-        text_renderer.render_center_viewport(text);
+        const float text_y_pos_delta = y_offset - 40.f -
+            progress_bar.get_height();
+        const float progress_bar_y_pos_delta = y_offset -
+            progress_bar.get_height();
 
-        ImGui::Core::Cursor::set_x(0.f);
-        ImGui::Core::Cursor::set_y_bottom_viewport(
-            -progress_bar.get_height());
-        progress_bar.set_width(ImGui::get_window_size().width);
-        progress_bar.render();
+        const auto set_and_render_progress_bar = [&]
+            {
+                ImGui::Core::Cursor::set_x(0.f);
+                progress_bar.set_width(ImGui::get_window_size().
+                    width);
+                progress_bar.render();
+            };
+
+        switch (layout_mode)
+        {
+        case LayoutMode::VIEWPORT:
+            ImGui::Core::Cursor::set_y_bottom_viewport(
+                text_y_pos_delta);
+            text_renderer.render_center_viewport(text);
+
+            ImGui::Core::Cursor::set_y_bottom_viewport(
+                progress_bar_y_pos_delta);
+            set_and_render_progress_bar();
+            break;
+
+        case LayoutMode::WINDOW:
+            ImGui::Core::Cursor::set_y_bottom_window(
+                text_y_pos_delta);
+            text_renderer.render_center_window(text);
+
+            ImGui::Core::Cursor::set_y_bottom_window(
+                progress_bar_y_pos_delta);
+            set_and_render_progress_bar();
+            break;
+        }
+    }
+
+    void ProgressDisplay::set_layout_mode(
+        LayoutMode new_layout_mode)
+    {
+        layout_mode = new_layout_mode;
     }
 
     void ProgressDisplay::set_text(const std::string& new_text)
     {
         text = new_text;
+    }
+
+    void ProgressDisplay::set_y_offset(float new_y_offset)
+    {
+        y_offset = new_y_offset;
     }
 
     void ProgressDisplay::increment(unsigned delta)
