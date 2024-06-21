2fb91d467afb44d214f82c74d3934e3f6627a58f
Rageking8 <tomleetyt@gmail.com>
2024-06-22 00:30:28 +0800

    Create `PLANAR_FORWARD_DECLARE_CLASS` helper macro

---
+++ Include/Planar/Engine/Core/Utils/Macros/DeclarationMacros.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+#define PLANAR_FORWARD_DECLARE_CLASS(ns, name) \
+    namespace Planar::ns                       \
+    {                                          \
+        class name;                            \
+    }                                          \
