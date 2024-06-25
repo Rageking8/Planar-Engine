2e9fd72570e80c2111200a04db2a4a0798279980
Rageking8 <tomleetyt@gmail.com>
2024-06-25 17:37:00 +0800

    Create macros for component definition

---
+++ Include/Planar/Engine/Component/ComponentMacros.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "Planar/Engine/Core/Utils/Macros/MacroOverload.hpp"
+#include "Planar/Engine/Component/ComponentType.hpp"
+
+#define PLANAR_DEFINE_COMPONENT_TYPE_AND_NAME(type)            \
+    static constexpr ComponentType TYPE = ComponentType::type; \
+    static constexpr const char* NAME = #type;                 \
+
+#define PLANAR_DEFINE_COMPONENT(...)                     \
+    PLANAR_DEFINE_MACRO_OVERLOAD(PLANAR_DEFINE_COMPONENT \
+        __VA_OPT__(,) __VA_ARGS__)                       \
+
+#define PLANAR_DEFINE_COMPONENT1(type)        \
+    PLANAR_DEFINE_COMPONENT2(type, Component) \
+
+#define PLANAR_DEFINE_COMPONENT2(type, parent)             \
+    using Parent = parent;                                 \
+    PLANAR_DEFINE_COMPONENT_TYPE_AND_NAME(type)            \
+                                                           \
+    virtual bool match(ComponentType other) const override \
+    {                                                      \
+        if (other == TYPE)                                 \
+        {                                                  \
+            return true;                                   \
+        }                                                  \
+                                                           \
+        return Parent::match(other);                       \
+    }                                                      \
