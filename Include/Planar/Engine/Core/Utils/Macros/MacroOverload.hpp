080cbaa45f426e1f80c10510a40c73ea2f3912e5
Rageking8 <tomleetyt@gmail.com>
2024-06-25 17:21:55 +0800

    Expand comments on usage for `PLANAR_DEFINE_MACRO_OVERLOAD` macro

--- Include/Planar/Engine/Core/Utils/Macros/MacroOverload.hpp
+++ Include/Planar/Engine/Core/Utils/Macros/MacroOverload.hpp
@@ -4,9 +4,21 @@
     _7, _8, _9, _10, _11, _12, name, ...) name               \
 
 // Supports argument length of range [0, 12]
-// Usage:
-//     #define MY_MACRO(...) \
-//         PLANAR_DEFINE_MACRO_OVERLOAD(MY_MACRO __VA_OPT__(,) __VA_ARGS__)
+// Create overloaded macro:
+//
+//     #define MY_MACRO(...)                                                \
+//         PLANAR_DEFINE_MACRO_OVERLOAD(MY_MACRO __VA_OPT__(,) __VA_ARGS__) \
+//
+// Define overloads by appending the number of parameters to the macro name:
+//
+//     #define MY_MACRO1(param1) ...
+//     #define MY_MACRO2(param1, param2) ...
+//
+// Invoke the overloaded macro:
+//
+//     MY_MACRO(arg1)
+//     MY_MACRO(arg1, arg2)
+//
 #define PLANAR_DEFINE_MACRO_OVERLOAD(name, ...)                      \
     PLANAR_GET_FUNCTION_NAME(_0 __VA_OPT__(,) __VA_ARGS__, name##12, \
         name##11, name##10, name##9, name##8, name##7, name##6,      \
