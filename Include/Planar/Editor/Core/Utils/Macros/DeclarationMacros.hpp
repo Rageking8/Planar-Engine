d14a3169bdf8c0171e48a0dbfc818ba3e8ae25da
Rageking8 <tomleetyt@gmail.com>
2024-06-25 21:11:21 +0800

    Add declaration macros for `Editor` and remove unneeded `Utils/Macros` folder

---
+++ Include/Planar/Editor/Core/Utils/Macros/DeclarationMacros.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "Planar/Engine/Core/Utils/Macros/DeclarationMacros.hpp"
+
+#define PLANAR_EDITOR_FORWARD_DECLARE_CLASS(ns, name)      \
+    PLANAR_FORWARD_DECLARE_CLASS(Planar::Editor::ns, name) \
