a5027a6db2077ea79c4cc7ead48ba1497f6dd267
Rageking8 <tomleetyt@gmail.com>
2024-06-27 19:19:38 +0800

    Rectify invalid condition in `get_pending_restore_default_layout` function

--- Source/Planar/Editor/UI/Window/SettingsWindow.cpp
+++ Source/Planar/Editor/UI/Window/SettingsWindow.cpp
@@ -32,7 +32,7 @@ namespace Planar::Editor::UI::Window
     {
         bool result = pending_restore_default_layout;
 
-        pending_restore_default_layout = result ? false :
+        pending_restore_default_layout = reset ? false :
             pending_restore_default_layout;
 
         return result;
