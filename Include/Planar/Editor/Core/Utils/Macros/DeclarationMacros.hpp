9eb1822a5d78bdde6f7e01edc7e4455e755fcf1d
Rageking8 <tomleetyt@gmail.com>
2024-06-27 15:32:33 +0800

    Create `PLANAR_EDITOR_FORWARD_DECLARE_EXPAND_NS` convenience macro

--- Include/Planar/Editor/Core/Utils/Macros/DeclarationMacros.hpp
+++ Include/Planar/Editor/Core/Utils/Macros/DeclarationMacros.hpp
@@ -2,5 +2,9 @@
 
 #include "Planar/Engine/Core/Utils/Macros/DeclarationMacros.hpp"
 
+#define PLANAR_EDITOR_FORWARD_DECLARE_EXPAND_NS(ns) \
+    Planar::Editor::ns                              \
+
 #define PLANAR_EDITOR_FORWARD_DECLARE_CLASS(ns, name)      \
-    PLANAR_FORWARD_DECLARE_CLASS(Planar::Editor::ns, name) \
+    PLANAR_FORWARD_DECLARE_CLASS(                          \
+        PLANAR_EDITOR_FORWARD_DECLARE_EXPAND_NS(ns), name) \
