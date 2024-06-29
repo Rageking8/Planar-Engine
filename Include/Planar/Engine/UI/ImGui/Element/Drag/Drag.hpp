b4e191e72de05ec2a9d89d4937a990fc9777ad4c
Rageking8 <tomleetyt@gmail.com>
2024-06-29 19:38:37 +0800

    Change return to `bool` for `drag_float` and `drag_float_2` functions

--- Include/Planar/Engine/UI/ImGui/Element/Drag/Drag.hpp
+++ Include/Planar/Engine/UI/ImGui/Element/Drag/Drag.hpp
@@ -8,12 +8,12 @@
 
 namespace Planar::Engine::UI::ImGui::Element::Drag
 {
-    PLANAR_API void drag_float(const std::string& label,
+    PLANAR_API bool drag_float(const std::string& label,
         float& value, float speed = 0.1f, float min = -FLT_MAX,
         float max = FLT_MAX, const std::string& format = "%.3f",
         int flags = 0);
 
-    PLANAR_API void drag_float_2(const std::string& label,
+    PLANAR_API bool drag_float_2(const std::string& label,
         std::array<float, 2>& value, float speed = 0.1f,
         float min = -FLT_MAX, float max = FLT_MAX,
         const std::string& format = "%.3f", int flags = 0);
--- Source/Planar/Engine/UI/ImGui/Element/Drag/Drag.cpp
+++ Source/Planar/Engine/UI/ImGui/Element/Drag/Drag.cpp
@@ -4,19 +4,19 @@
 
 namespace Planar::Engine::UI::ImGui::Element::Drag
 {
-    void drag_float(const std::string& label, float& value,
+    bool drag_float(const std::string& label, float& value,
         float speed, float min, float max,
         const std::string& format, int flags)
     {
-        ::ImGui::DragFloat(label.c_str(), &value, speed, min,
+        return ::ImGui::DragFloat(label.c_str(), &value, speed, min,
             max, format.c_str(), flags);
     }
 
-    void drag_float_2(const std::string& label,
+    bool drag_float_2(const std::string& label,
         std::array<float,2>& value, float speed, float min,
         float max, const std::string & format, int flags)
     {
-        ::ImGui::DragFloat2(label.c_str(), value.data(), speed,
+        return ::ImGui::DragFloat2(label.c_str(), value.data(), speed,
             min, max, format.c_str(), flags);
     }
 }
