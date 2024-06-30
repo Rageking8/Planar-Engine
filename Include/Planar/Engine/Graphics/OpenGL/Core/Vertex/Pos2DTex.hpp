d49698b1064000d58a21752e8012674382130e11
Rageking8 <tomleetyt@gmail.com>
2024-06-30 22:35:12 +0800

    Create vertex type `Pos2DTex`

---
+++ Include/Planar/Engine/Graphics/OpenGL/Core/Vertex/Pos2DTex.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+namespace Planar::Engine::Graphics::OpenGL::Core::Vertex
+{
+    struct Pos2DTex
+    {
+        float x{};
+        float y{};
+        float u{};
+        float v{};
+    };
+}
