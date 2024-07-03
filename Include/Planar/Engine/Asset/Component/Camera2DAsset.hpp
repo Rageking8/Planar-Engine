e38a06355cea3fb80a82cda6f00ceee8fb4252e4
Rageking8 <tomleetyt@gmail.com>
2024-07-03 11:05:04 +0800

    Add `Active` mixin to `Camera2DAsset`

--- Include/Planar/Engine/Asset/Component/Camera2DAsset.hpp
+++ Include/Planar/Engine/Asset/Component/Camera2DAsset.hpp
@@ -3,13 +3,15 @@
 #include "Planar/Engine/Export/ExportMacros.hpp"
 #include "Planar/Engine/Asset/Component/ComponentAsset.hpp"
 #include "Planar/Engine/Core/Utils/Macros/DeclarationMacros.hpp"
+#include "Planar/Engine/Asset/Component/Mixin/Active.hpp"
 
 PLANAR_ENGINE_FORWARD_DECLARE_CLASS(Component, Camera2D)
 
 namespace Planar::Engine::Asset::Component
 {
     class PLANAR_API Camera2DAsset :
-        public ComponentAsset<Engine::Component::Camera2D>
+        public ComponentAsset<Engine::Component::Camera2D,
+        Mixin::Active>
     {
     public:
         Camera2DAsset();
