a8f9cd80903ad9169ccb10568042894a63d51fac
Rageking8 <tomleetyt@gmail.com>
2024-07-06 13:33:50 +0800

    Create new `Active` mixin for engine `Component`

---
+++ Include/Planar/Engine/Component/Mixin/Active.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "Planar/Engine/Core/Mixin/Mixin.hpp"
+#include "Planar/Engine/Core/Utils/Macros/DeclarationMacros.hpp"
+
+PLANAR_FORWARD_DECLARE_CLASS(YAML, Node)
+
+namespace Planar::Engine::Component::Mixin
+{
+    template <typename AssetT>
+    class Active : public Core::Mixin::Mixin<AssetT>
+    {
+    public:
+        using Core::Mixin::Mixin<AssetT>::Mixin;
+
+        void load(YAML::Node node);
+
+        bool get_active() const;
+        void set_active(bool new_active);
+
+    private:
+        bool active;
+    };
+}
+
+#include "Planar/Engine/Component/Mixin/Active.tpp"
---
+++ Include/Planar/Engine/Component/Mixin/Active.tpp
@@ -0,0 +1,23 @@
+#include "Planar/Engine/Component/Mixin/Active.hpp"
+
+namespace Planar::Engine::Component::Mixin
+{
+    template <typename AssetT>
+    inline void Active<AssetT>::load(YAML::Node node)
+    {
+        set_active(this->asset->get_active());
+    }
+
+    template <typename AssetT>
+    inline bool Active<AssetT>::get_active() const
+    {
+        return active;
+    }
+
+    template <typename AssetT>
+    inline void Active<AssetT>::set_active(bool new_active)
+    {
+        active = new_active;
+        this->asset->set_active(new_active);
+    }
+}
