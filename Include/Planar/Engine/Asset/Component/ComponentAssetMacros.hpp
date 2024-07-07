1c2cd2845d18a68e16c36aca6ec49b51141f4705
Rageking8 <tomleetyt@gmail.com>
2024-07-07 12:12:55 +0800

    Implement macros for defining component asset functions

---
+++ Include/Planar/Engine/Asset/Component/ComponentAssetMacros.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#define PLANAR_DEFINE_COMPONENT_ASSET_GET_SET_VALUE(   \
+    component, type, name, key)                        \
+    type component##Asset::get_##name() const          \
+    {                                                  \
+        return get_value<type>(#key);                  \
+    }                                                  \
+                                                       \
+    void component##Asset::set_##name(type new_##name) \
+    {                                                  \
+        set_value(#key, new_##name);                   \
+    }                                                  \
+
+#define PLANAR_DEFINE_COMPONENT_ASSET_GET_SET_FLOAT_2(            \
+    component, type, name, key1, key2, key3)                      \
+    type component##Asset::get_##name() const                     \
+    {                                                             \
+        return get_float_2(#key1, #key2, #key3);                  \
+    }                                                             \
+                                                                  \
+    void component##Asset::set_##name(type new_##name)            \
+    {                                                             \
+        set_float_2(#key1, #key2, #key3, new_##name.get_array()); \
+    }                                                             \
