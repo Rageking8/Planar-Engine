ee784b2e6069d929878464f9681794cb553a22c9
Rageking8 <tomleetyt@gmail.com>
2024-07-09 16:33:12 +0800

    Make element component macros more terse using `PLANAR_DEFINE_GET_SET_RAW`

--- Include/Planar/Editor/UI/Element/Component/Core/ComponentMacros.hpp
+++ Include/Planar/Editor/UI/Element/Component/Core/ComponentMacros.hpp
@@ -1,25 +1,19 @@
 #pragma once
 
+#include "Planar/Engine/Core/Utils/Macros/DefinitionMacros.hpp"
+
 #define PLANAR_DEFINE_ELEMENT_COMPONENT_GET_SET_DRAG_1( \
     component, name)                                    \
-    float component::get_##name() const                 \
-    {                                                   \
-        return name.get_x();                            \
-    }                                                   \
+    PLANAR_DEFINE_GET_SET_RAW(component, float, name,   \
+        return name.get_x();,                           \
                                                         \
-    void component::set_##name(float new_##name)        \
-    {                                                   \
         name.set_x(new_##name);                         \
-    }                                                   \
+    )                                                   \
 
 #define PLANAR_DEFINE_ELEMENT_COMPONENT_GET_SET_DRAG_2( \
     component, type, name)                              \
-    type component::get_##name() const                  \
-    {                                                   \
-        return name.get_value();                        \
-    }                                                   \
+    PLANAR_DEFINE_GET_SET_RAW(component, type, name,    \
+        return name.get_value();,                       \
                                                         \
-    void component::set_##name(type new_##name)         \
-    {                                                   \
         name.set_value(new_##name.get_array());         \
-    }                                                   \
+    )                                                   \
