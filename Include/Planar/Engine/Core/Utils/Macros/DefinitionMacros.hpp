b310f8538b44e4044b4a16b3e2817d369ea97ff0
Rageking8 <tomleetyt@gmail.com>
2024-07-09 16:31:41 +0800

    Add `PLANAR_DEFINE_GET_SET_RAW` to `DefinitionMacros`

--- Include/Planar/Engine/Core/Utils/Macros/DefinitionMacros.hpp
+++ Include/Planar/Engine/Core/Utils/Macros/DefinitionMacros.hpp
@@ -1,15 +1,25 @@
 #pragma once
 
+#include "Planar/Engine/Core/Utils/Macros/MacroUtils.hpp"
+
+#define PLANAR_DEFINE_GET_SET_RAW(type, get_set_type, name, \
+    get_content, set_content)                               \
+    get_set_type type::get_##name() const                   \
+    {                                                       \
+        get_content                                         \
+    }                                                       \
+                                                            \
+    void type::set_##name(get_set_type new_##name)          \
+    {                                                       \
+        set_content                                         \
+    }                                                       \
+
 #define PLANAR_DEFINE_GET_SET(type, get_set_type, name, \
     get_content, set_content)                           \
-    get_set_type type::get_##name() const               \
-    {                                                   \
+    PLANAR_DEFINE_GET_SET_RAW(type, get_set_type, name, \
         get_content                                     \
-        return name;                                    \
-    }                                                   \
+        return name;,                                   \
                                                         \
-    void type::set_##name(get_set_type new_##name)      \
-    {                                                   \
-        name = new_##name;                              \
+        name = PLANAR_CONCAT(new_, name);               \
         set_content                                     \
-    }                                                   \
+    )                                                   \
