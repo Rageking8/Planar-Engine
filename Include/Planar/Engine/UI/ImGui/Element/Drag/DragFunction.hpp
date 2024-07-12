d55e27ed649c98f8f84f75a14f7fd35ff5d40658
Rageking8 <tomleetyt@gmail.com>
2024-07-12 18:59:15 +0800

    Add `drag_int` and `drag_int_2` functions

--- Include/Planar/Engine/UI/ImGui/Element/Drag/DragFunction.hpp
+++ Include/Planar/Engine/UI/ImGui/Element/Drag/DragFunction.hpp
@@ -8,10 +8,18 @@
 
 namespace Planar::Engine::UI::ImGui::Element::Drag
 {
-    PLANAR_API bool drag_float(const std::string& label,
-        float& value, float speed = 0.1f, float min = -FLT_MAX,
-        float max = FLT_MAX, const std::string& format = "%.3f",
+    PLANAR_API bool drag_int(const std::string& label, int& value,
+        float speed = 1.f, int min = 0, int max = 0,
+        const std::string& format = "%d", int flags = 0);
+
+    PLANAR_API bool drag_int_2(const std::string& label,
+        std::array<int, 2>& value, float speed = 1.f,
+        int min = 0, int max = 0, const std::string& format = "%d",
         int flags = 0);
+
+    PLANAR_API bool drag_float(const std::string& label, float& value,
+        float speed = 0.1f, float min = -FLT_MAX, float max = FLT_MAX,
+        const std::string& format = "%.3f", int flags = 0);
 
     PLANAR_API bool drag_float_2(const std::string& label,
         std::array<float, 2>& value, float speed = 0.1f,
--- Source/Planar/Engine/UI/ImGui/Element/Drag/DragFunction.cpp
+++ Source/Planar/Engine/UI/ImGui/Element/Drag/DragFunction.cpp
@@ -4,6 +4,22 @@
 
 namespace Planar::Engine::UI::ImGui::Element::Drag
 {
+    bool drag_int(const std::string& label, int& value,
+        float speed, int min, int max, const std::string& format,
+        int flags)
+    {
+        return ::ImGui::DragInt(label.c_str(), &value, speed, min,
+            max, format.c_str(), flags);
+    }
+
+    bool drag_int_2(const std::string& label,
+        std::array<int, 2>& value, float speed, int min,
+        int max, const std::string& format, int flags)
+    {
+        return ::ImGui::DragInt2(label.c_str(), value.data(), speed,
+            min, max, format.c_str(), flags);
+    }
+
     bool drag_float(const std::string& label, float& value,
         float speed, float min, float max,
         const std::string& format, int flags)
