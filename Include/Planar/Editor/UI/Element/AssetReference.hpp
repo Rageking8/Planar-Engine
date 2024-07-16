e8d215bee4f1b1f2cffa64b2e594b3919a42d5e8
Rageking8 <tomleetyt@gmail.com>
2024-07-16 22:05:01 +0800

    Implement new `AssetReference` editor element

---
+++ Include/Planar/Editor/UI/Element/AssetReference.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "Planar/Engine/UI/ImGui/Element/InputField.hpp"
+
+#include <string>
+#include <functional>
+
+namespace Planar::Editor::UI::Element
+{
+    class AssetReference :
+        public Engine::UI::ImGui::Element::InputField
+    {
+    public:
+        AssetReference(const std::string& name, const std::string& type,
+            const std::function<void(std::string)>& update_text_callback,
+            float right_offset = 20.f, float x_pos = 100.f);
+
+        virtual void render() override;
+
+        std::string get_asset() const;
+        void set_asset(const std::string& new_asset,
+            bool skip_text_update = false, bool skip_modified = false);
+
+    private:
+        std::string type;
+        std::string asset;
+        std::function<void(std::string)> update_text_callback;
+    };
+}
---
+++ Source/Planar/Editor/UI/Element/AssetReference.cpp
@@ -0,0 +1,61 @@
+#include "Planar/Editor/UI/Element/AssetReference.hpp"
+#include "Planar/Engine/UI/ImGui/Core/Size/Width.hpp"
+#include "Planar/Engine/UI/ImGui/ImGui.hpp"
+
+#include <optional>
+
+namespace Planar::Editor::UI::Element
+{
+    AssetReference::AssetReference(const std::string& name,
+        const std::string& type,
+        const std::function<void(std::string)>& update_text_callback,
+        float right_offset, float x_pos) : InputField(name, name,
+        { Engine::UI::ImGui::Core::Size::Width::WidthMode::FILL,
+        0.f, 0.f, right_offset }, x_pos, true), type{ type },
+        update_text_callback{ update_text_callback }
+    {
+
+    }
+
+    void AssetReference::render()
+    {
+        InputField::render();
+
+        std::optional<std::string> drop_result =
+            Engine::UI::ImGui::drag_drop_target(type);
+        if (drop_result)
+        {
+            set_asset(*drop_result);
+        }
+    }
+
+    std::string AssetReference::get_asset() const
+    {
+        return asset;
+    }
+
+    void AssetReference::set_asset(const std::string& new_asset,
+        bool skip_text_update, bool skip_modified)
+    {
+        asset = new_asset;
+
+        if (!skip_modified)
+        {
+            set_modified();
+        }
+
+        if (skip_text_update)
+        {
+            return;
+        }
+
+        if (asset.empty())
+        {
+            clear_text();
+        }
+        else
+        {
+            update_text_callback(asset);
+        }
+    }
+}
