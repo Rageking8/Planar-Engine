6d4e62f8530643e0fe24580a4fa6705025dd82f5
Rageking8 <tomleetyt@gmail.com>
2024-07-18 23:23:57 +0800

    Make native exports for `load_sprite` in `AssetDatabase`

---
+++ Include/Planar/Engine/Export/Planar/Engine/Asset/AssetDatabase.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "Planar/Engine/Export/ExportMacros.hpp"
+
+#include <cstddef>
+
+PLANAR_EXPORT_DECLARE_HANDLE_FUNCTION(
+    Planar_Engine_Asset_AssetDatabase_load_sprite,
+    void, const char* name, const char* asset,
+    const unsigned char* sprite, std::size_t length)
---
+++ Source/Planar/Engine/Export/Planar/Engine/Asset/AssetDatabase.cpp
@@ -0,0 +1,7 @@
+#include "Planar/Engine/Export/Planar/Engine/Asset/AssetDatabase.hpp"
+#include "Planar/Engine/Asset/AssetDatabase.hpp"
+
+PLANAR_EXPORT_DEFINE_HANDLE_FUNCTION(Planar_Engine_Asset_AssetDatabase,
+    load_sprite, const char* name, name, const char* asset,
+    asset, const unsigned char* sprite, sprite, std::size_t length,
+    length, Planar::Engine::Asset::AssetDatabase)
