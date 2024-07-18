379779308d99cd2808f8698a3327f1e5a4b3b626
Rageking8 <tomleetyt@gmail.com>
2024-07-18 23:37:01 +0800

    Add managed `AssetDatabase` class

---
+++ Script/Planar/Engine/Asset/AssetDatabase.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Runtime.InteropServices;
+
+using Planar.Engine.Core;
+
+namespace Planar.Engine.Asset;
+
+public class AssetDatabase
+{
+    private IntPtr _handle;
+
+    public AssetDatabase(IntPtr handle)
+    {
+        _handle = handle;
+    }
+
+    public void LoadSprite(string name, string asset, byte[] sprite)
+    {
+        Planar_Engine_Asset_AssetDatabase_load_sprite(_handle,
+            name, asset, sprite);
+    }
+
+    [DllImport(ImportConstants.PlanarEngineDLL)]
+    private static extern void
+        Planar_Engine_Asset_AssetDatabase_load_sprite(IntPtr handle,
+        string name, string asset, byte[] sprite);
+}
