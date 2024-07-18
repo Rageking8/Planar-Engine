1078d16d0712be220bc68a475f856d8f3928af3b
Rageking8 <tomleetyt@gmail.com>
2024-07-18 23:52:29 +0800

    Complete scene setup and asset loading in `Entry`

--- Script/Planar/Engine/Core/Entry.cs
+++ Script/Planar/Engine/Core/Entry.cs
@@ -1,6 +1,10 @@
 using System;
+using System.IO;
+using System.Collections.Generic;
 using System.Runtime.InteropServices;
 
+using Planar.Engine;
+using Planar.Engine.Asset;
 using Planar.Engine.Core.Log;
 
 using static Planar.Engine.Core.Native.NativeFunction;
@@ -25,6 +29,31 @@ public class Entry
             Console.ReadLine();
 
             return;
+        }
+
+        List<string> sceneAssets =
+            GetResourceNamesByExtension(".planarscene");
+        Scene.Scene mainScene = application.CreateScene(sceneAssets[0]);
+        application.LoadScene(mainScene);
+
+        List<string> spriteAssets =
+            GetResourceNamesByExtension(".planarsprite");
+        HashSet<string> pngAssets =
+            new(GetResourceNamesByExtension(".png"));
+
+        AssetDatabase assetDatabase = application.GetAssetDatabase();
+
+        foreach (string sprite in spriteAssets)
+        {
+            string current = Path.ChangeExtension(sprite, ".png");
+
+            if (pngAssets.Contains(current))
+            {
+                string name = current[..^4];
+                name = name[(name.LastIndexOf('.') + 1)..];
+                assetDatabase.LoadSprite(name, ReadResource(sprite),
+                    ReadResourceBytes(current));
+            }
         }
 
         application.Run();
