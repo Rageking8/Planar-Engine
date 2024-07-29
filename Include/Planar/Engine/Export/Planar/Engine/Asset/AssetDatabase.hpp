7c3aecec4a8cb0bdb5ff792a7cf48f7757bd4399
Rageking8 <tomleetyt@gmail.com>
2024-07-29 10:59:22 +0800

    Create native exports for `load_audio` in `AssetDatabase` type

--- Include/Planar/Engine/Export/Planar/Engine/Asset/AssetDatabase.hpp
+++ Include/Planar/Engine/Export/Planar/Engine/Asset/AssetDatabase.hpp
@@ -8,3 +8,7 @@ PLANAR_EXPORT_DECLARE_HANDLE_FUNCTION(
     Planar_Engine_Asset_AssetDatabase_load_sprite,
     void, const char* name, const char* asset,
     const unsigned char* sprite, std::size_t length)
+PLANAR_EXPORT_DECLARE_HANDLE_FUNCTION(
+    Planar_Engine_Asset_AssetDatabase_load_audio,
+    void, const char* name, const char* asset,
+    const unsigned char* audio, std::size_t length)
--- Source/Planar/Engine/Export/Planar/Engine/Asset/AssetDatabase.cpp
+++ Source/Planar/Engine/Export/Planar/Engine/Asset/AssetDatabase.cpp
@@ -5,3 +5,7 @@ PLANAR_EXPORT_DEFINE_HANDLE_FUNCTION(Planar_Engine_Asset_AssetDatabase,
     load_sprite, const char* name, name, const char* asset,
     asset, const unsigned char* sprite, sprite, std::size_t length,
     length, Planar::Engine::Asset::AssetDatabase)
+PLANAR_EXPORT_DEFINE_HANDLE_FUNCTION(Planar_Engine_Asset_AssetDatabase,
+    load_audio, const char* name, name, const char* asset,
+    asset, const unsigned char* audio, audio, std::size_t length,
+    length, Planar::Engine::Asset::AssetDatabase)
